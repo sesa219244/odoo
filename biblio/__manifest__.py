e1f727631c2feea1267ece9f72943ba719798996
sesa219244 <122741934+sesa219244@users.noreply.github.com>
2023-01-22 18:16:17 +0100

    Update __manifest__.py

--- biblio/__manifest__.py
+++ biblio/__manifest__.py
@@ -8,8 +8,8 @@
     'description': """
         """,
 
-    'author': "Oscar Garijo",
-    'website': "http://www.odoo.com",
+    'author': "",
+    'website': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
